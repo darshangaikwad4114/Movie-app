071f3f96bc766c80c3fc31caf26caf0ac3aa8c6e
darshan <darshangaikwad4114@gmail.com>
2025-02-09 23:57:11 +0530

    Add axios and env packages, implement local storage for favourites, and update component props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
@@ -6,20 +7,48 @@ import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
 import AddFavourites from "./components/AddFavourites";
 import RemoveFavourites from "./components/RemoveFavourites";
+import PropTypes from "prop-types";
+
+const API_KEY = "e04fd151"; // OMDB API key
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error("Error reading local storage", error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error("Error setting local storage", error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [favourites, setFavourites] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
+  const [favourites, setFavourites] = useLocalStorage("react-movie-app-favourites", []);
+  const [searchValue, setSearchValue] = useState("Avengers");
 
   const getMovieRequest = async (searchValue) => {
-    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=e04fd151`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=${API_KEY}`;
 
-    const response = await fetch(url);
-    const responseJson = await response.json();
-
-    if (responseJson.Search) {
-      setMovies(responseJson.Search);
+    try {
+      const response = await axios.get(url);
+      if (response.data.Search) {
+        setMovies(response.data.Search);
+      }
+    } catch (error) {
+      console.error("Error fetching data from OMDB API", error);
     }
   };
 
@@ -27,24 +56,9 @@ const App = () => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
-  useEffect(() => {
-    const movieFavourites = JSON.parse(
-      localStorage.getItem("react-movie-app-favourites")
-    );
-
-    if (movieFavourites) {
-      setFavourites(movieFavourites);
-    }
-  }, []);
-
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem("react-movie-app-favourites", JSON.stringify(items));
-  };
-
   const addFavouriteMovie = (movie) => {
     const newFavouriteList = [...favourites, movie];
     setFavourites(newFavouriteList);
-    saveToLocalStorage(newFavouriteList);
   };
 
   const removeFavouriteMovie = (movie) => {
@@ -53,7 +67,6 @@ const App = () => {
     );
 
     setFavourites(newFavouriteList);
-    saveToLocalStorage(newFavouriteList);
   };
 
   return (
@@ -67,6 +80,7 @@ const App = () => {
           movies={movies}
           handleFavouritesClick={addFavouriteMovie}
           favouriteComponent={AddFavourites}
+          className="col-12 col-md-6 mb-4"
         />
       </div>
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -77,10 +91,20 @@ const App = () => {
           movies={favourites}
           handleFavouritesClick={removeFavouriteMovie}
           favouriteComponent={RemoveFavourites}
+          className="col-12 col-md-6 mb-4"
         />
       </div>
     </div>
   );
+};
+
+App.propTypes = {
+  movies: PropTypes.array,
+  favourites: PropTypes.array,
+  searchValue: PropTypes.string,
+  setSearchValue: PropTypes.func,
+  addFavouriteMovie: PropTypes.func,
+  removeFavouriteMovie: PropTypes.func,
 };
 
 export default App;
--- src/components/AddFavourites.js
+++ src/components/AddFavourites.js
@@ -3,17 +3,18 @@ import React from "react";
 const AddFavourite = () => {
   return (
     <>
-      <span className="mr-5 mx-5">Add to Favourite </span>
+      <span className="mr-5 mx-5">Add to Favourite</span>
       <svg
         width="1em"
         height="1em"
         viewBox="0 0 16 16"
-        class="bi bi-heart-fill"
+        className="bi bi-heart-fill"
         fill="red"
         xmlns="http://www.w3.org/2000/svg"
+        aria-label="Add to Favourite"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
         />
       </svg>
